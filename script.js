8be4913f540e6a5913a276498888f04f0f9928c5
Dustin LaMarr <dustin.lamarr@gmail.com>
2020-10-27 10:02:06 -0600

    finished functionality of quick add. Added JS to add quick add movie to unwatched list

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var searchButton = document.querySelector("#search-button");
-var searchInput = docment.querySelector("#search-input")
+var searchInput = document.querySelector("#search-input");
+var unwatchedMovieLi = document.querySelector("#movie-uw-list");
 
 
 //Api call OMDB, IMDB
@@ -8,17 +9,40 @@ var searchInput = docment.querySelector("#search-input")
 //TMDB  (images)
 var queryTMDB = "https://api.themoviedb.org/3/movie/{movie_id}/images?api_key=77cf51e8d01e26a06a5030f3b856fe9e&language=en-US"
 //OMDB API
-var queryOMDB = "https://www.omdbapi.com/?t=" + movie + "&apikey=10e7754b";;
 
 // Perfoming an AJAX GET request to our queryURL
 searchButton.addEventListener("click", function(event){
 event.preventDefault();
 movieAdd = searchInput.value.trim();
+console.log(movieAdd);
+var queryOMDB = "https://www.omdbapi.com/?t=" + movieAdd + "&apikey=10e7754b";
 
-})
+
 $.ajax({
   url: queryOMDB,
   method: "GET"
+})
+  .then(function (response){
+console.log(response); 
+var quickAdd = response.Type;
+console.log(quickAdd);
+
+$.ajax({
+  url: queryOMDB,
+  method: "GET"
+})
+
+    if(quickAdd === "movie") {
+      var newMovie = document.createElement('li');
+      newMovie.setAttribute("class", "list-group-item");
+      var moviePoster = document.createElement('img');
+      moviePoster.setAttribute("src", "")
+     newMovie.textContent = response.Title;
+     unwatchedMovieLi.appendChild(newMovie);
+     var 
+    }
+});
+});
 //Event listener (quick add button)
 //Array for next ten list (with movie posters, use bootstrap carousel)
 // $('.carousel').carousel({
