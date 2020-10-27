42b3cb6ae7b17d8fd6980f71cf51dd6c5eb73b07
Dustin LaMarr <dustin.lamarr@gmail.com>
2020-10-27 07:21:01 -0600

    Added event listener to search button

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
+var searchButton = document.querySelector("#search-button");
+var searchInput = docment.querySelector("#search-input")
+
+
 //Api call OMDB, IMDB
+
+
 //TMDB  (images)
-var queryTmdb = "https://api.themoviedb.org/3/movie/{movie_id}/images?api_key=77cf51e8d01e26a06a5030f3b856fe9e&language=en-US"
+var queryTMDB = "https://api.themoviedb.org/3/movie/{movie_id}/images?api_key=77cf51e8d01e26a06a5030f3b856fe9e&language=en-US"
 //OMDB API
-var queryOmdb = "https://www.omdbapi.com/?t=" + movie + "&apikey=10e7754b";;
+var queryOMDB = "https://www.omdbapi.com/?t=" + movie + "&apikey=10e7754b";;
 
 // Perfoming an AJAX GET request to our queryURL
+searchButton.addEventListener("click", function(event){
+event.preventDefault();
+movieAdd = searchInput.value.trim();
+
+})
 $.ajax({
-  url: queryTmdb,
+  url: queryOMDB,
   method: "GET"
 //Event listener (quick add button)
 //Array for next ten list (with movie posters, use bootstrap carousel)
