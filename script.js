da0edb16f2653220b12e2e2d5d668f4f57db679a
Travis Mendenhall <twmwayne@gmail.com>
2020-10-27 20:20:04 -0600

    built time calculator, function to add movies to unwatched and watched lists

--- script.js
+++ script.js
@@ -3,6 +3,45 @@ var searchInput = document.querySelector("#search-input");
 var unwatchedMovie = document.querySelector("#movies-unwatched");
 var apiKey = "77cf51e8d01e26a06a5030f3b856fe9e"
 
+//global variables to make arrays from movies chosen
+var cntr = 0
+var names = new Array();
+var runtimes = new Array();
+var runningTotal = new Array();
+var timeTotal = 0;
+
+//function to convert minutes to hours, minutes.
+function timeConvert(n) {
+  var num = n;
+  var hours = (num / 60);
+  var rhours = Math.floor(hours);
+  var minutes = (hours - rhours) * 60;
+  var rminutes = Math.round(minutes);
+  return rhours.toString() + " hour(s) and " + rminutes.toString() + " minute(s).";
+  }
+
+function addToWatched(index) {
+  var title = names[index];
+
+//
+  if (runningTotal.indexOf(title) == -1) {
+    var tm = runtimes[index];
+    var isrt = "<label>" + title + "</label>"
+    var movieDiv = $("<div class='movie'>");
+    var totalStr;
+
+    movieDiv.append(isrt);
+    $("#movies-watched").prepend(movieDiv);
+    runningTotal.push(title);
+    timeTotal += tm;
+    totalStr = timeConvert(timeTotal);
+    document.getElementById("time-watched-graphic").innerHTML = totalStr;
+
+  }
+
+}
+
+
 searchButton.addEventListener("click", function (event) {
   event.preventDefault();
   movieAdd = searchInput.value.trim();
@@ -10,9 +49,13 @@ searchButton.addEventListener("click", function (event) {
   var queryOMDB = "https://www.omdbapi.com/?t=" + movieAdd + "&apikey=10e7754b";
   var queryTMDB = "https://api.themoviedb.org/3/search/multi?api_key=" + apiKey + "&language=en-US&query=" + movieAdd + "&include_adult=false"
 
+
+
+
+
   // https://api.themoviedb.org/3/search/movie?api_key=<<api_key>>&language=en-US&page=1&include_adult=false
 
-// Perfoming an AJAX GET request to our queryURL
+  // Perfoming an AJAX GET request to our queryURL
   $.ajax({
     url: queryOMDB,
     method: "GET"
@@ -22,14 +65,43 @@ searchButton.addEventListener("click", function (event) {
       var quickAdd = responseOMDB.Type;
 
       if (quickAdd === "movie") {
-        var newMovie = document.createElement('row');
+        //var newMovie = document.createElement('row');
+        var tm = responseOMDB.Runtime;
+        var st1 = "test"
+        var rt_int = 0;
+
+        st1 = tm;
+        var pos = st1.indexOf(" ");
+
+        if (pos > -1) {
+          st1 = st1.substr(0, pos);
+          rt_int = parseInt(st1);
+        }
+        
+        //alert(tm);
         // newMovie.setAttribute("class", "list-group-item");
-        newMovie.setAttribute("id", "new-movie-row")
-        newMovie.textContent = responseOMDB.Title;
-        unwatchedMovie.appendChild(newMovie);
+        // newMovie.setAttribute("id", "new-movie-row")
+        // newMovie.Runtime = responseOMDB.Runtime;
+        // newMovie.textContent = responseOMDB.Title;
+        // unwatchedMovie.appendChild(newMovie);
+
+        var movieDiv = $("<div class='movie'>");
+
+        //button on movies unwatched to add to movies watched
+
+        var title = responseOMDB.Title;
+        var isrt = "<label>" + title + "</label> <input type='button' value='add to watched' onclick='addToWatched(" + cntr + ")'/>"
+        movieDiv.append(isrt);
+
+        // Putting the entire movie above the previous movies
+        $("#movies-unwatched").prepend(movieDiv);
+
+        names.push(title);
+        runtimes.push(rt_int);
+        cntr++;
       }
 
-// we need to add IF statement for Series 
+      // we need to add IF statement for Series 
 
       $.ajax({
         url: queryTMDB,
@@ -48,9 +120,9 @@ searchButton.addEventListener("click", function (event) {
           newMovie.appendChild(moviePosterDiv);
           moviePosterDiv.appendChild(moviePoster);
         })
-        // need to get the image to append to the row/ (want the img, Title, Rating, and Rotten Tomato Score)  
+      // need to get the image to append to the row/ (want the img, Title, Rating, and Rotten Tomato Score)  
 
-})
+    })
 });
 
 
